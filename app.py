13a023d464d8bf9cc1c82bbb84fa8e7601c767f5
john saidi <jsaidi@email.sc.edu>
2022-11-17 19:09:40 -0500

    Fifa 22 player stats, First commit

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, request, url_for, redirect
+from dbquery import get_info_by_playername, get_players_by_teamName, get_players_by_nationalTeam, get_players_by_tName_pName, get_players_by_clubName_nationality, get_all_playernames, get_all_nation_names
+
+
+
+app = Flask(__name__)
+
+# implemet player search by nationality, club name and player profile
+@app.route('/', methods=['GET','POST'])
+def root():
+    all_nations = get_all_nation_names()
+
+    if request.method == "POST":
+        teamname = request.form.get('search')
+        if all_nations:
+            for teams in all_nations:
+                if teamname in teams['nationality_name']:
+                    return redirect(url_for('player_by_country', nation=teamname))
+        else:
+            # search database for player names
+            player_info = get_info_by_playername(teamname)    
+            for players in player_info:
+                if teamname in players['short_name']:
+                    return redirect(url_for('profile', player=teamname))
+
+ 
+    return render_template('form.html')
+
+# returns all players players
+@app.route('/playerz')
+def allplayers():
+    all_players = get_all_playernames()
+
+    if all_players:
+        if request.method == "POST":
+            teamname = request.form['search']
+            # if teamname["nationality_name"] in all_nations:
+            return render_template(url_for('profile')) 
+    return render_template('playertable.html', p_name=all_players)
+
+@app.route('/player_profile/<clubname>/<playername>/')
+def profile(clubname, playername):
+    player_info = get_players_by_tName_pName(clubname, playername)
+    return render_template('index.html', p_name=player_info)
+
+# All countries
+@app.route('/countries')
+def allcountries():
+    all_nations = get_all_nation_names()
+    return render_template('nations.html', p_name=all_nations)
+
+# players from the same nation
+@app.route('/players/nationality/<nation>')
+def player_by_country(nation):
+    players = get_players_by_nationalTeam(nation)
+    return render_template('playertable.html', p_name=players)
+
+# players from the same football club
+@app.route('/players/club-name/<clubname>')
+def player_by_club(clubname):
+    players = get_players_by_teamName(clubname)
+    return render_template('playertable.html', p_name=players)
+
+# implement player stats comparison with numpy/Matplotlib
+# Dont forget error checking
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ dbquery.py
@@ -0,0 +1,39 @@
+from cs50 import SQL
+
+
+db = SQL("sqlite:///fifa22data.db")
+
+def get_all_playernames():
+    return db.execute("SELECT short_name, long_name, club_joined, club_name, overall, player_face_url FROM fifa22data")
+
+def get_info_by_playername(playername):
+    return db.execute("SELECT * FROM fifa22data WHERE short_name = :short_name", short_name=playername)
+
+def get_players_by_teamName(team_name):
+    return db.execute(f"SELECT * FROM fifa22data WHERE club_name = '{team_name}'")
+
+#filter players by team and player names
+def get_players_by_tName_pName(team_name, playername):
+    return db.execute(f"SELECT * FROM fifa22data WHERE club_name = '{team_name}'AND short_name= '{playername}'")
+
+#filter players by club_name and nationality_name
+def get_players_by_clubName_nationality(clubname, nationality):
+    return db.execute(f"SELECT * FROM fifa22data WHERE club_name = '{clubname}'AND nationality_name= '{nationality}'")
+
+def get_all_nation_names():
+    return db.execute("SELECT DISTINCT nationality_name FROM fifa22data ORDER BY nationality_name ASC")
+
+
+def get_players_by_nationalTeam(team_name):
+    return db.execute(f"SELECT * FROM fifa22data WHERE nationality_name = '{team_name}'")
+
+# def get_stats_by_playername(playername):
+#     return db.execute("SELECT short_name, overall, pace, shooting, passing, }dribbling, defending, physic  FROM fifa22data WHERE short_name = :short_name", short_name=playername)
+
+# def get_playernames_by_clubTeamn(clubTeam):
+#     return db.execute("SELECT short_name FROM fifa22data WHERE club_name = :club_name", club_name=clubTeam)
+
+# def get_playernames_by_nationalTeam(nationalTeam):
+#     return db.execute("SELECT short_name FROM fifa22data WHERE nationality_name = :nationality_name", nationality_name=nationalTeam)
+
+
